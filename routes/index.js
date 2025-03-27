dcb33bce498cc54f96ce8b39e0608a4b5a947c6c
omegus100 <121273757+omegus100@users.noreply.github.com>
2025-03-26 18:16:55 -0700

    Index route added

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', async (req, res) =>{
+    res.send("Index page")
+    // let books 
+    // try {
+    //     books = await Book.find().sort({ createdAt: 'desc'}).limit(10).exec()
+    // } catch {
+    //     books = []
+    // }
+    // res.render('index', {books: books})
+})
+
+module.exports = router
