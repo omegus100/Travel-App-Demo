f84ecbf767a1543a9eb5b788ba7b2a44f0c09b76
omegus100 <121273757+omegus100@users.noreply.github.com>
2025-03-26 17:55:03 -0700

    Connected

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const express = require('express')
 const app =  express()
 const expressLayouts = require('express-ejs-layouts')
 
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/authors')
+// const indexRouter = require('./routes/index')
+// const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -21,7 +21,7 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))    
 
-app.use('/', indexRouter)
-app.use('/authors', authorRouter)
+// app.use('/', indexRouter)
+// app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
